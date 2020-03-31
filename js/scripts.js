32d02fad083e13ee72be161d38235aa6a5b997b6
austinb847 <austinb847@icloud.com>
2020-03-31 16:40:14 -0700

    add if else blockto determine what to display on page based on what the user entered in the form

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $("form#stress_test").submit(function(event) {
     event.preventDefault();
-    
+    $("form#stress_test").hide();
    
     var warningSigns = $("input:checkbox[name=warning-signs]:checked").map(function() {
       return $(this).val();
@@ -15,6 +15,21 @@ $(document).ready(function() {
       return $(this).val();
     });
 
+   if (healthSigns.length === 4 && warningSigns.length === 6) {
+     $(".output").html("<p>" + "Check out these books to help you!" + "</p>" +
+                      "<ul>" +
+                        "<li>" + "Declutter your mind" + "</li>" +
+                        "<li>" + "Stress Free Living" + "</li>");
+                                                     
+   } else if (warningSigns.length === 4 && stressManagements.length === 5) {
+     $(".output").html("<p>" + "You're doing a great job at taking action to help your stress!" + "</p>");
+   
+    } else if(healthSigns.length > 4) {
+      $(".output").html("<p>" + "Please see a doctor!" + "</p>");
+
+    } else {
+      $(".output").html("<p>" + "Stress sucks! You're not alone.");
+    }
    
 
 
